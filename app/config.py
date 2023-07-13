0599583222ba81e7af0f32956435e857427c0720
MustSave <dbstjdgk1541@gmail.com>
2023-07-14 00:06:08 +0900

    Feat: load model for inference

--- app/config.py
+++ app/config.py
@@ -1,12 +1,19 @@
+from os.path import join
+from os import environ
 from typing import Any
+
+from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 from app.constants import Environment
-from os.path import join, dirname
+from app.llm.constants import LLMModel
 
 
 class Config(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=join(dirname(dirname(__file__)), ".env"), env_file_encoding="utf-8"
+        env_file=join(environ["ROOT_DIR"], ".env"),
+        env_file_encoding="utf-8",
+        use_enum_values=True,
     )
 
     REDIS_URL: str
@@ -19,6 +26,13 @@ class Config(BaseSettings):
     CORS_HEADERS: list[str]
 
     APP_VERSION: str
+
+    PEFT_MODEL_DIR: str | LLMModel
+
+    @field_validator("PEFT_MODEL_DIR")
+    def validate_model_dir(cls, v):
+        assert v in LLMModel._member_map_, "invalid model name"
+        return LLMModel._member_map_.get(v)
 
 
 settings = Config()
--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from typing import Any, Callable
+from threading import Thread
 
 import orjson
+import torch
+
 from pydantic import BaseModel, root_validator
+from transformers import StoppingCriteria
 
 
 def orjson_dumps(v: Any, *, default: Callable[[Any], Any] | None) -> str:
@@ -15,6 +19,15 @@ def convert_datetime_to_gmt(dt: datetime) -> str:
         dt = dt.replace(tzinfo=ZoneInfo("KTC"))
 
     return dt.strftime("%Y-%m-%dT%H:%M:%S%z")
+
+
+class SingletonMetaClass(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 
 class ORJSONMoel(BaseModel):
@@ -31,3 +44,52 @@ class ORJSONMoel(BaseModel):
         }
 
         return {**data, **datetime_fields}
+
+
+class StoppingCriteriaSub(StoppingCriteria):
+    def __init__(self, stops=None, encounters=1):
+        super().__init__()
+        self.stops = [stop.to("cuda") for stop in stops] if stops else []
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor):
+        for stop in self.stops:
+            if torch.all((stop == input_ids[0][-len(stop) :])).item():
+                return True
+
+        return False
+
+
+class LoadedLLM(metaclass=SingletonMetaClass):
+    isLoaded = False
+    isRunning = False
+
+    def __call__(cls, model, tokenizer, *args, **kwargs):
+        from transformers import TextIteratorStreamer
+
+        cls.model = model
+        cls.tokenizer = tokenizer
+        cls.isLoaded = True
+        cls.streamer = TextIteratorStreamer(
+            tokenizer, timeout=10, skip_prompt=True, skip_special_tokens=True
+        )
+        cls.stopping_criteria = StoppingCriteriaSub()
+
+    def generate(cls, history, x):
+        generate_kwargs = dict(
+            **cls.tokenizer(
+                f"{history}Human: {x}\nRemon:", return_tensors="pt", return_token_type_ids=False
+            ).to(0),
+            streamer=cls.streamer,
+            max_new_tokens=512,
+            do_sample=True,
+            early_stopping=True,
+            eos_token_id=2,
+            stopping_criteria=cls.stopping_criteria,
+            temperature=0.1,
+            # top_p=top_p,
+            # top_k=top_k
+        )
+        thread = Thread(target=cls.model.generate, kwargs=generate_kwargs)
+        thread.start()
+
+        return cls.streamer
