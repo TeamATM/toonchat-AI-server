e92388bdf0c771de56ad7322eb11abf18a533c67
MustSave <dbstjdgk1541@gmail.com>
2023-10-31 15:28:00 +0900

    Feat: update transformers library

--- app/llm/prompter.py
+++ app/llm/prompter.py
@@ -48,3 +48,28 @@ class ToonchatV23Prompter(HuggingfacePrompter):
             )
         tmp.append("### Assistant: ")
         return " ".join(tmp)
+
+
+class ChatMlPrompter(HuggingfacePrompter):
+    def get_prompt(self, messages: PromptData) -> str:
+        tmp = []
+        tmp.append(
+            "<|im_start|>system\n당신은 주어진 캐릭터의 말투, 성격을 모방하여 대답하는 챗봇입니다. 유저의 질문에 대하여 캐릭터의 특징을 최대한 살려 대답하세요.<|im_end|>"
+        )
+        if len(messages.get_persona()) > 1:
+            tmp.append(
+                f"<|im_start|>system\n아래는 당신의 페르소나입니다. 유저의 질문에 대하여 다음의 페르소나를 고려하여 적절한 대답을 완성하세요. {messages.get_persona()}<|im_end|>"
+            )
+        if isinstance(messages.get_reference(), list) and len(messages.get_reference()[0]) > 1:
+            tmp.append(
+                f"<|im_start|>system\n다음은 유저의 질문과 관련된 소설의 내용입니다. 유저의 질문에 대해 다음의 내용을 바탕으로 적절한 대답을 완성하세요. {messages.get_reference()[0]}<|im_end|>"
+            )
+        for i, message in enumerate(messages.get_chat_history_list()):
+            if i == 0 and not message.fromUser:
+                continue
+            # assert i == 0 or (i != 0 and message.fromUser != messages.get_chat_history_list()[i-1].fromUser)
+            tmp.append(
+                f"<|im_start|>{'user' if message.fromUser else 'assistant'}\n{message.content}<|im_end|>"
+            )
+        tmp.append("<|im_start|>assistant\n")
+        return "\n".join(tmp)
